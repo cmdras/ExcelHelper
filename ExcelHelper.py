16114b4d89b862757b56220350a38b0a681ed6c8
chris <cmdras94@gmail.com>
2019-01-20 17:09:35 +0100

    Added some try/except structures

--- ExcelHelper.py
+++ ExcelHelper.py
@@ -1,7 +1,6 @@
 from External import xlsx2csv
-import traceback, datetime, csv
+import traceback, datetime, csv, os
 import time
-
 
 def GetDataFromXlsx(xlsxFile, complete=True, headers=False):
     start = time.time()
@@ -19,8 +18,10 @@ def __ConvertToCsv(xlsxFileName, excelFile, sheets):
     csvs = []
     date = datetime.datetime.now()
     datestr = date.strftime("%Y%m%d%H%M")
+    if not os.path.isdir("/tmp"):
+        os.mkdir("tmp")
     for sheetId in sheets:
-        csvName = r'{}{}{}{}{}{}'.format('tmp/', xlsxFileName, sheetId, '-', datestr, '.csv')
+        csvName = os.path.join('tmp', r'{}{}{}{}{}'.format(xlsxFileName, sheetId, '-', datestr, '.csv'))
         try:
             excelFile.convert(csvName, sheetId)
             print 'Successfully converted {}, sheet {} to csv!'.format(xlsxFileName, sheetId)
@@ -32,22 +33,30 @@ def __ConvertToCsv(xlsxFileName, excelFile, sheets):
 
 def __CombineDataFromCsvs(csvs):
     data = []
-    for csvFile in csvs:
-        with open(csvFile, 'r') as csvreader:
-            reader = csv.reader(csvreader)
-            data.append([row for row in reader])
+    try:
+        for csvFile in csvs:
+            with open(csvFile, 'r') as csvreader:
+                reader = csv.reader(csvreader)
+                data.append([row for row in reader])
+    except Exception as e:
+        print 'An error occured while combining data from the sheets: '
+        print traceback.format_exc()
     return data
-
 
 def __ParseSheetData(data, headers=False):
     sheetCounter = 1
     result = {}
-    for sheet in data:
-        if not sheet:
-            continue
-        sheetStr = "Sheet{}".format(sheetCounter)
-        headerData = sheet.pop(0) if headers else ["col{}".format(i) for i in xrange(len(sheet[0]))]
-        result[sheetStr] = __GetColumnData(headerData, sheet)
+    try:
+        for sheet in data:
+            if not sheet:
+                continue
+            sheetStr = "Sheet{}".format(sheetCounter)
+            headerData = sheet.pop(0) if headers else ["col{}".format(i) for i in xrange(len(sheet[0]))]
+            result[sheetStr] = __GetColumnData(headerData, sheet)
+    except Exception as e:
+        print 'An error occured while parsing the sheet data: '
+        print traceback.format_exc()
+    
     return result
               
 def __GetColumnData(headerData, sheet):
